3d64cb6e7c073acd4b753245153b7c1739444e9b
Bit Shift <bitshift@bigmacintosh.net>
2014-07-10 07:55:09 +0000

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./web.nix
+    ];
+
+  boot = {
+    cleanTmpDir = true;
+    loader.grub = {
+      enable = true;
+      version = 1;
+      extraPerEntryConfig = "root (hd0)";
+      device = "nodev";
+    };
+  };
+
+  networking = {
+    hostName = "eos";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 443 ];
+      allowedUDPPortRanges = [ { from = 60000; to = 61000; }  # mosh
+                             ];
+    };
+  };
+
+  time.timeZone = "UTC";
+
+  i18n = {
+    consoleFont = "lat9w-16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_GB.UTF-8";
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # -env -qaP | grep wget
+  #
+  # dummied out with syntax because it makes me uneasy, but I want to know what it was set as if that passes
+  ## environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
+    pkgs.wget
+    pkgs.vim
+  ];
+
+  services = {
+    openssh = {
+      enable = true;
+      ports = [ 9022 ];
+      passwordAuthentication = false;
+    };
+    sshd.usePAM = false;
+
+    locate = {
+       enable = true;
+       period = "15 8 * * *";  # 8:15 am UTC, should be low-activity time
+    };
+  };
+
+  users = {
+    mutableUsers = false;
+    extraUsers = {
+      root = {
+        passwordFile = "/root/.passwd";
+        hashedPassword = null;  # must be unset because security.initialRootPassword implies it, and it supersedes passwordFile 
+      };
+      bitshift = {
+        name = "bitshift";
+        uid = 1000;
+        group = "users";
+        extraGroups = [ "wheel" ];
+        createHome = true;
+        home = "/home/bitshift";
+        useDefaultShell = true;
+        openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3PyHwNf19brgX6dFQFYp2cnQO0n0vEdiHuW71lWzsql4C4oH1QUclI2WCdc+rpugiPuDlTJfGjy1CoDNYFCBXGo56nR+JBcEQk+EPsZ6UnbwwxUruzKkuPK8N3pIKU/IZq8QzebBh1pX6VQSa3P22QfJ7IqddRurC/WghsLgZOCquu+Qgj+q1BZhEsgj/QZZ5q6E8s0t7zsUZQdjRwWSjjtvUDGGOIm3ABkJa9hVZ8S5QNnnHQEnpr+4U0fXaWJ5Uze3ZwpeqiubsAAE7nKH/TA6mnPggZV0AS5YaHPFdRCfMjCrmy6Twue6/gVo5UBCdC1W83qY0NOZRPgakc1Bt bitshift@ubuntu"
+                                        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDed7UE/5R9HRRUVTGoO5hly1bt+4hbE16zptCUcO40VhNx7KlVIY+aYE1tCkAmFnCqemu7axaSLcyFhTHrKRsdMa3oFyhj84Y1wwOVog2bEk5xqM0LUOGPuM3X5BsOoX3dJTsi16hq73vSLNuvye5ctaSP+9H3aODqkI18hGBO5Zgrj9LVg33ZyLmq4wQXFAwmWBW22BLwsM4bIs/59Ea2VXegarBHL4FbaL6Rt7OyVT168tdGFLQZcxH2ph2SIbfv1L+D+8cUwKlE5zPLv03mYqulnt/DhvBubZ78NvYVjbNRv35YmmlDjYDvNYkzlBj3yRiDFlRXSZPbzHz9WMKV JuiceSSH"
+                                      ];
+      };
+    };
+    defaultUserShell = "/run/current-system/sw/bin/bash";
+  };
+
+  security = {
+    sudo.wheelNeedsPassword = false;
+  };
+}
---
+++ simple-nginx.nix
@@ -0,0 +1,154 @@
+{ lib, ... } :
+
+let fcgiParams = ''
+    fastcgi_param   QUERY_STRING            $query_string;
+    fastcgi_param   REQUEST_METHOD          $request_method;
+    fastcgi_param   CONTENT_TYPE            $content_type;
+    fastcgi_param   CONTENT_LENGTH          $content_length;
+     
+    fastcgi_param   SCRIPT_FILENAME         $request_filename;
+    fastcgi_param   SCRIPT_NAME             $fastcgi_script_name;
+    fastcgi_param   REQUEST_URI             $request_uri;
+    fastcgi_param   DOCUMENT_URI            $document_uri;
+    fastcgi_param   DOCUMENT_ROOT           $document_root;
+    fastcgi_param   SERVER_PROTOCOL         $server_protocol;
+     
+    fastcgi_param   GATEWAY_INTERFACE       CGI/1.1;
+    fastcgi_param   SERVER_SOFTWARE         nginx/$nginx_version;
+     
+    fastcgi_param   REMOTE_ADDR             $remote_addr;
+    fastcgi_param   REMOTE_PORT             $remote_port;
+    fastcgi_param   SERVER_ADDR             $server_addr;
+    fastcgi_param   SERVER_PORT             $server_port;
+    fastcgi_param   SERVER_NAME             $server_name;
+     
+    fastcgi_param   HTTPS                   $https if_not_empty;
+     
+    # PHP only, required if PHP was built with --enable-force-cgi-redirect
+    fastcgi_param   REDIRECT_STATUS         200;
+  '';
+in rec {
+  fastcgiParams = fcgiParams;
+
+  serveSites = sites :
+    let makeConfig = { hostname, extraHostnames ? [], nginxBaseConf, phpRule ? true, usePhp ? false, ssl ? null, ... } :
+        let serverNames = lib.concatStringsSep " " (lib.singleton hostname ++ extraHostnames);
+            mainPort = if ssl == null
+                          then "80"
+                          else "443 ssl";
+            portAnnot = if hostname == "_"
+                           then " default"
+                           else "";
+            sslRedirect = if ssl == null
+                             then ""
+                             else ''
+                                    server {
+                                      listen 80${portAnnot};
+                                      server_name ${serverNames};
+                                      return 301 https://$server_name$request_uri;  # enforce https
+                                    }
+                                  '';
+            sslConfig = if ssl == null
+                           then ""
+                           else ''
+                                  ssl_certificate ${ssl.cert};
+                                  ssl_certificate_key ${ssl.key};
+                                '';
+            phpBlock = let phpRuleInner = if usePhp
+                                             then ''
+                                                    try_files $uri =404;
+                                                    ${fcgiParams}
+                                                    fastcgi_pass unix:/run/phpfpm/nginx;
+                                                  ''
+                                             else "return 403;";
+                       in if phpRule
+                          then ''
+                                 location ~ \.php$ {
+                                   ${phpRuleInner}
+                                 }
+                               ''
+                          else "";
+        in ''
+	  ${sslRedirect}
+
+          server {
+            ${sslConfig}
+
+            listen ${mainPort}${portAnnot};
+            server_name ${serverNames};
+            root /srv/www/${if hostname == "_" then "default" else hostname};
+            index ${if usePhp then "index.php " else ""}index.html index.htm;
+            
+            ${phpBlock}
+
+            ${nginxBaseConf}
+          }
+        '';
+    in {
+      services.nginx = {
+        enable = true;
+        user = "www-data";
+        group = "www-data";
+        config = ''
+          events {
+            use epoll;
+          }
+          http {
+            ${lib.concatMapStrings makeConfig sites}
+          }
+        '';
+      };
+
+      users.extraUsers."www-data" = {
+        uid = 33;
+        group = "www-data";
+        home = "/srv/www";
+        createHome = true;
+        useDefaultShell = true;
+      };
+      users.extraGroups."www-data".gid = 33;
+
+      services.phpfpm.poolConfigs.nginx = ''
+        listen = /run/phpfpm/nginx
+        listen.owner = www-data
+        listen.group = www-data
+        listen.mode = 0660
+        user = www-data
+        pm = dynamic
+        pm.max_children = 75
+        pm.start_servers = 10
+        pm.min_spare_servers = 5
+        pm.max_spare_servers = 20
+        pm.max_requests = 500
+
+        php_flag[display_errors] = on
+        php_admin_value[error_log] = /run/phpfpm/php-fpm.log
+        php_admin_flag[log_errors] = on
+        php_value[date.timezone] = "UTC";
+      '';
+    };
+
+  # site types - for now one, later maybe more
+  basicSite = hostname : extraHostnames : extraConf : {
+    hostname = hostname;
+    extraHostnames = extraHostnames;
+    nginxBaseConf = extraConf;
+  };
+
+  # modifiers - possible todo: inverse operations
+  withPhp = site : site // {
+    usePhp = true;
+  };
+  withoutDefaultPhpRule = site : site // {
+    phpRule = false;
+  };
+  withSsl = cert : key : site : site // {
+    ssl = {
+      cert = cert;
+      key = key;
+    };
+  };
+
+  # convenience functions to avoid parenthitis
+  withCustomPhp = site : withoutDefaultPhpRule (withPhp site);
+}
