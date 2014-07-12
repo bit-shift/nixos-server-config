419176ef2f78cf79a00136bcd1e83e5ecba35c56
Bit Shift <bitshift@bigmacintosh.net>
2014-07-12 02:17:01 +0000

    More hmm.

--- simple-nginx.nix
+++ simple-nginx.nix
@@ -160,6 +160,7 @@ in rec {
       "~ \\.php\$" = (lib.remove "return 403;" site.locs."~ \\.php\$")
                   ++ [ "try_files $uri =404;"
                        "${fcgiParams}"
+                       "fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;"
                        "fastcgi_pass unix:/run/phpfpm/nginx;" ];
     };
   };
