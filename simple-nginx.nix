d27eea34e5fc3c12c8cd12471a5a8847acddcd82
Bit Shift <bitshift@bigmacintosh.net>
2014-07-12 05:19:15 +0000

    H5AI support, take 2 part 2 chapter 2.

--- simple-nginx.nix
+++ simple-nginx.nix
@@ -8,7 +8,7 @@ in rec {
   phpSimpleRules = [ "/_h5ai/server/php/index.php" = [ "try_files $uri =404;"
                      "${fcgiParams}"
                      "fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;"
-                     "fastcgi_pass unix:/run/phpfpm/nginx;" ];
+                     "fastcgi_pass unix:/run/phpfpm/nginx;"
                    ];
 
   serveSites = addHosts : sites :
