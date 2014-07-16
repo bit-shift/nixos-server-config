79a33ad81eafd5a98c5939b2768eb820655d3a06
Bit Shift <bitshift@bigmacintosh.net>
2014-07-16 09:49:12 +0000

    Oh, it POSTs...

--- rtmp/auth.php.nix
+++ rtmp/auth.php.nix
@@ -1,6 +1,6 @@
 user : pass : ''
   <?php
-  if(empty($_GET['user']) || empty($_GET['pass'])) {
+  if(empty($_POST['user']) || empty($_POST['pass'])) {
     header('HTTP/1.1 400 Bad Request');
     die('Invalid query.');
   } else if(strcmp($_GET['user'], '${user}')==0 && strcmp($_GET['pass'], '${pass}')==0) {
