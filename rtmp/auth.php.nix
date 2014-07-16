1297715dc2ff8d597bd29afd245c3a352cfa725d
Bit Shift <bitshift@bigmacintosh.net>
2014-07-16 09:59:40 +0000

    That was pretty dumb.

--- rtmp/auth.php.nix
+++ rtmp/auth.php.nix
@@ -3,7 +3,7 @@ user : pass : ''
   if(empty($_POST['user']) || empty($_POST['pass'])) {
     header('HTTP/1.1 400 Bad Request');
     die('Invalid query.');
-  } else if(strcmp($_GET['user'], '${user}')==0 && strcmp($_GET['pass'], '${pass}')==0) {
+  } else if(strcmp($_POST['user'], '${user}')==0 && strcmp($_POST['pass'], '${pass}')==0) {
     echo('Great! Valid user/pass!');
   } else {
     header('HTTP/1.1 403 Forbidden');
